d30df7b2a77517a47cba1c6ed492ea9783c1bf87
Sifat <105747804+sifat371@users.noreply.github.com>
2026-09-26 05:12:12 +0600

    Update app test for renamed import action

--- frontend/tests/App.test.tsx
+++ frontend/tests/App.test.tsx
@@ -89,7 +89,7 @@ test('adds a created case without discarding dirty edits when selection is decli
   await user.upload(screen.getByLabelText(/DWI NIfTI/i), new File(['dwi'], 'dwi.nii.gz'));
   await user.upload(screen.getByLabelText(/ADC NIfTI/i), new File(['adc'], 'adc.nii.gz'));
   await user.upload(screen.getByLabelText(/FLAIR NIfTI/i), new File(['flair'], 'flair.nii.gz'));
-  await user.click(screen.getByRole('button', { name: 'Create Case' }));
+  await user.click(screen.getByRole('button', { name: 'Import Case' }));
   await screen.findByRole('button', { name: /created case/i });
   expect(window.confirm).toHaveBeenCalled();
   expect(screen.getByRole('button', { name: /case one/i })).toHaveAttribute('aria-pressed', 'true');
