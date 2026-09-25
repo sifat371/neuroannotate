1af17855275d431b75b8028d6215a0484822c05b
Sifat <105747804+sifat371@users.noreply.github.com>
2026-09-26 05:11:32 +0600

    Update app test for DICOM-first import UI

--- frontend/tests/App.test.tsx
+++ frontend/tests/App.test.tsx
@@ -85,6 +85,7 @@ test('adds a created case without discarding dirty edits when selection is decli
   await screen.findByRole('button', { name: /case one/i });
   await act(async () => { useWorkspace.getState().setDirty(true); });
   await user.type(screen.getByLabelText('Case name'), 'Created case');
+  await user.click(screen.getByRole('button', { name: /NIfTI triad/i }));
   await user.upload(screen.getByLabelText(/DWI NIfTI/i), new File(['dwi'], 'dwi.nii.gz'));
   await user.upload(screen.getByLabelText(/ADC NIfTI/i), new File(['adc'], 'adc.nii.gz'));
   await user.upload(screen.getByLabelText(/FLAIR NIfTI/i), new File(['flair'], 'flair.nii.gz'));
